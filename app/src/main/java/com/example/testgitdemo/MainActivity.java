2d5ab00f16d7810e4e7d045ce0fb6f078264906b
mac.xiao <15268343037@163.com>
2019-09-26 23:53:01 +0800

    test2

--- app/src/main/java/com/example/testgitdemo/MainActivity.java
+++ app/src/main/java/com/example/testgitdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // test
+    // test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
