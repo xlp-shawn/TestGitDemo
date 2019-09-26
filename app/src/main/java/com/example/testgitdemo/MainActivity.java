c84aa1566817b91d2832395acd8785b5078227b2
mac.xiao <15268343037@163.com>
2019-09-27 00:08:33 +0800

    test05

--- app/src/main/java/com/example/testgitdemo/MainActivity.java
+++ app/src/main/java/com/example/testgitdemo/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     // test
     // test2
+    // test05
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
